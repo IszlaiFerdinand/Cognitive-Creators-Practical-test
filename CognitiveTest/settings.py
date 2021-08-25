12a3fef2394ad1a8e4e90ef420d4a31b2fdffefd
Iszlai Ferdinand <ferdinandiszlai@gmail.com>
2021-08-25 14:45:30 +0300

    Setting up database connection

--- CognitiveTest/settings.py
+++ CognitiveTest/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'CognitiveTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CognitiveCreatorsTest',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306'
     }
 }
 
