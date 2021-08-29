97bd7b3f9071e8b58d77157b31ac79d281648eb0
Iszlai Ferdinand <ferdinandiszlai@gmail.com>
2021-08-29 22:29:44 +0300

    Totalamount and remove functions bugfixes

--- Checkout/views.py
+++ Checkout/views.py
@@ -11,14 +11,26 @@ def Checkout(request):
         userID = request.user.id
         cart = CartItems.objects.select_related(
             'product').filter(user_id=userID)
-        return render(request, 'Checkout.html', {'cart': cart})
+        totalamount = 0
+        for item in cart.iterator():
+            totalamount += item.quantity*item.product.price
+
+        totalwithVAT = totalamount*1.19
+        return render(request, 'Checkout.html', {'cart': cart, 'totalamount': totalamount, 'totalwithVAT': totalwithVAT})
     else:
         return redirect('/')
 
 
 def Removefromcart(request):
     cartitems_id = request.GET['remove']
+    quantityobj = CartItems.objects.filter(id=cartitems_id).get()
+    quantity = quantityobj.quantity
+    product_idobj = CartItems.objects.filter(id=cartitems_id).get()
+    product_id = product_idobj.product_id
+    # adding back to stock removed products
+    updateproduct = Product.objects.get(id=product_id)
+    updateproduct.onstock += quantity
+    updateproduct.save()
+    # deleting selected cart item from database
     CartItems.objects.filter(id=cartitems_id).delete()
-    print(cartitems_id)
-    print("Success")
     return views.Checkout(request)
